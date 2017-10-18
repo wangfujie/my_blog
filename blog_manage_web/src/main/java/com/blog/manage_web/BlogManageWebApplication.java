6fd7b20d8e144c2d31f5168777970079fc3f0c05
wangfujie <784727590@qq.com>
2017-10-18 17:10:56 +0800

    maven多模块初始化

---
+++ blog_manage_web/src/main/java/com/blog/manage_web/BlogManageWebApplication.java
@@ -0,0 +1,12 @@
+package com.blog.manage_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogManageWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogManageWebApplication.class, args);
+	}
+}
---
+++ blog_pojo/src/main/java/com/blog/pojo/BlogPojoApplication.java
@@ -0,0 +1,12 @@
+package com.blog.pojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPojoApplication.class, args);
+	}
+}
