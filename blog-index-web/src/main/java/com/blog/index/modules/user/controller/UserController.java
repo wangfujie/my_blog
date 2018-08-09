b53c441e4155033883e653b8e37014e39b2480e7
wangfj <784727590@qq.com>
2018-08-09 18:01:39 +0800

    增加mybatis模块，逆向工程

--- blog-index-web/src/main/java/com/blog/index/modules/user/controller/UserController.java
+++ blog-index-web/src/main/java/com/blog/index/modules/user/controller/UserController.java
@@ -1,18 +1,28 @@
 package com.blog.index.modules.user.controller;
 
+import com.blog.index.modules.user.service.IBlogUserService;
 import com.blog.pojo.entity.BlogUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author wangfujie
- * @version 1.0.0
- * @email wangfj@chinawiserv.com
+ * @date 2018-08-09 15:32
+ * @description 用户相关，控制层
  */
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private IBlogUserService blogUserService;
+
+    @GetMapping("/getBlogUserList")
+    public Object getBlogUserList(){
+        return blogUserService.getBlogUserList();
+    }
 
     @GetMapping("/hello")
     public String hello(){
--- blog-pojo/src/main/java/com/blog/pojo/entity/BlogUser.java
+++ blog-pojo/src/main/java/com/blog/pojo/entity/BlogUser.java
@@ -1,36 +1,51 @@
 package com.blog.pojo.entity;
 
+import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableName;
+import java.io.Serializable;
+
 /**
- * @author wangfujie
- * @version 1.0.0
- * @email wangfj@chinawiserv.com
+ * @author wangfj
+ * @date 2018-08-09
+ * @description
  */
-public class BlogUser {
-    private String uuid;
-    private String name;
-    private String email;
+@TableName("blog_user")
+public class BlogUser extends Model<BlogUser> {
 
-    public String getUuid() {
-        return uuid;
-    }
+    private static final long serialVersionUID = 1L;
 
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
+	private String uuid;
+	private String name;
+	private String email;
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getUuid() {
+		return uuid;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	protected Serializable pkVal() {
+		return this.uuid;
+	}
+
 }
