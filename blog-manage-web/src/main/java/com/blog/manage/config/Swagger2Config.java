5286b38523044e7534f2bd829eb078a3dfabca6b
wangfj <784727590@qq.com>
2018-09-12 18:09:48 +0800

    后台管理，修改swagger配置

--- blog-manage-web/src/main/java/com/blog/manage/config/Swagger2Config.java
+++ blog-manage-web/src/main/java/com/blog/manage/config/Swagger2Config.java
@@ -26,7 +26,7 @@ public class Swagger2Config {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.blog.index"))
+                .apis(RequestHandlerSelectors.basePackage("com.blog.manage"))
                 .paths(PathSelectors.any())
                 .build();
     }
