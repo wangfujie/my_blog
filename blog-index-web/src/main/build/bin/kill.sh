3b9b41cef360d7603bf02cbce49525940af173a3
wangfujie <784727590@qq.com>
2018-10-23 22:00:20 +0800

    打包配置增加，启动脚本，配置文件打包在外面等

---
+++ blog-index-web/src/main/build/bin/kill.sh
@@ -0,0 +1,7 @@
+APP_NAME=blog-index-web
+
+tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+if [ ${tpid} ]; then
+    echo 'Kill Process!'
+    kill -9 $tpid
+fi
---
+++ blog-manage-web/src/main/build/bin/kill.sh
@@ -0,0 +1,7 @@
+APP_NAME=blog-manage-web
+
+tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+if [ ${tpid} ]; then
+    echo 'Kill Process!'
+    kill -9 $tpid
+fi
