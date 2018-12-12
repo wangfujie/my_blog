c0265c4ccf2635482b09d7f1ea21eb5ba07a9b38
wangfj <784727590@qq.com>
2018-12-12 10:09:44 +0800

    去掉格式化注解，去掉无用引用空行等

--- blog-pojo/src/main/java/com/blog/pojo/entity/BlogFriendlyLinks.java
+++ blog-pojo/src/main/java/com/blog/pojo/entity/BlogFriendlyLinks.java
@@ -1,14 +1,12 @@
 package com.blog.pojo.entity;
 
-import com.baomidou.mybatisplus.enums.IdType;
-import java.util.Date;
-import com.baomidou.mybatisplus.annotations.TableId;
-import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.annotations.TableName;
-import com.fasterxml.jackson.annotation.JsonFormat;
-
+import com.baomidou.mybatisplus.enums.IdType;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @author wangfj
--- blog-pojo/src/main/java/com/blog/pojo/entity/BlogWebInfo.java
+++ blog-pojo/src/main/java/com/blog/pojo/entity/BlogWebInfo.java
@@ -1,14 +1,12 @@
 package com.blog.pojo.entity;
 
-import java.util.Date;
-import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.annotations.TableName;
 import com.baomidou.mybatisplus.enums.IdType;
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @author wangfj
